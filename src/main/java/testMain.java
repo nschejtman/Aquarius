a55d849c2314d29ab77f62805f6408a828fbda21
Franco Testori <franco.testori@ing.austral.edu.ar>
2014-04-23 01:45:16 -0300

    :Me lo dijo una gitana...me lo dijo con fevor... el Login es de primera porque Franco es un campeón

--- src/main/java/testMain.java
+++ src/main/java/testMain.java
@@ -5,12 +5,16 @@ import control.dao.UserDAO;
 
 public class testMain {
     public static void main(String[] args) {
-        User user = new User();
-        user.setUserName("Domingo");
-        user.setPassword("Concha");
+        User user1 = new User();
+        user1.setUserName("franco.testori@hotmail.com");
+        user1.setPassword("test1");
+        User user2 = new User();
+        user2.setUserName("nschejtman@hotmail.com");
+        user2.setPassword("test2");
 
 
-        UserDAO.addUser(user);
+        UserDAO.addUser(user1);
+        UserDAO.addUser(user2);
 
     }
 }
