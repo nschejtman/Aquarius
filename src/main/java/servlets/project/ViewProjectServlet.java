e2f64bada14e4b2977bbf033963c15ce866086a0
Franco Testori <franco.testori@ing.austral.edu.ar>
2014-07-16 14:00:50 -0300

    commit previo a un pull

--- src/main/java/servlets/project/ViewProjectServlet.java
+++ src/main/java/servlets/project/ViewProjectServlet.java
@@ -6,11 +6,13 @@ import model.Project;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "ViewProjectServlet", urlPatterns = "/secured/project")
 public class ViewProjectServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
